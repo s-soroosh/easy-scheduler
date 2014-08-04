49f8e6502164476d9544920a63cc5dc1ba4cc9f0
soroosh <soroosh.sarabadani@gmail.com>
2014-08-04 13:13:15 +0430

    download_url removed

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
+
 setup(
-  name = 'easy-scheduler',
-  packages = ['easy-scheduler'], # this must be the same as the name above
-  version = '0.1',
-  description = 'An easy and lighweight scheduler',
-  author = 'Soroosh Sarabadani',
-  author_email = 'soroosh.sarabadani@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
-  keywords = ['scheduler', 'thread base scheduler', 'process based scheduler'],
-  classifiers = [],
+    name='easy-scheduler',
+    packages=['easy-scheduler'],  # this must be the same as the name above
+    version='0.1',
+    description='An easy and lighweight scheduler',
+    author='Soroosh Sarabadani',
+    author_email='soroosh.sarabadani@gmail.com',
+    url='https://github.com/peterldowns/mypackage',
+    keywords=['scheduler', 'thread base scheduler', 'process based scheduler'],
+    classifiers=[],
 )
 
