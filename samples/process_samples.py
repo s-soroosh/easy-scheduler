780f77c5a836b3f13b477f9639054f68f3e4d52e
soroosh <soroosh.sarabadani@gmail.com>
2014-08-04 13:31:18 +0430

    samples refactored correspond to api

--- samples/process_samples.py
+++ samples/process_samples.py
@@ -1,4 +1,4 @@
-from easy_scheduler.simple_scheduler import ProcessSimpleScheduler
+from easy_scheduler.simple_scheduler import SimpleProcessScheduler
 
 seconds = 10  # We want to do something on every 10 seconds
 
@@ -7,5 +7,5 @@ def do_it():
     print 'Printed from a thread based scheduler'
 
 
-scheduler = ProcessSimpleScheduler(seconds, do_it, deamon=False)
+scheduler = SimpleProcessScheduler(seconds, do_it, deamon=False)
 scheduler.run()
--- samples/thread_samples.py
+++ samples/thread_samples.py
@@ -1,4 +1,4 @@
-from easy_scheduler.simple_scheduler import ThreadSimpleScheduler
+from easy_scheduler.simple_scheduler import SimpleThreadScheduler
 
 seconds = 10  # We want to do something on every 10 seconds
 
@@ -7,5 +7,5 @@ def do_it():
     print 'Printed from a thread based scheduler'
 
 
-scheduler = ThreadSimpleScheduler(seconds, do_it, deamon=False)
+scheduler = SimpleThreadScheduler(seconds, do_it, deamon=False)
 scheduler.run()
