5339a9f95df6c3df5b7509a952e78d3094ed27f3
soroosh <soroosh.sarabadani@gmail.com>
2014-08-05 16:37:04 +0430

    0.1.2 prepared

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='easy-scheduler',
-    packages=['easy_scheduler'],  # this must be the same as the name above
-    version='0.1.1',
+    packages=['easy_scheduler'],
+    version='0.1.2',
     description='An easy and lighweight scheduler',
     author='Soroosh Sarabadani',
     author_email='soroosh.sarabadani@gmail.com',
